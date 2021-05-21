96c30eb1e70a01c6c8b422ae5539600a5b029654
ajaychaturvedi <chaturvedi.ajay85@gmail.com>
2021-05-21 08:41:58 +0530

    simeple graph

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, send_file, render_template
+import io
+import base64
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+fig, ax = plt.subplots(figsize=(6, 6))
+ax = sns.set_style(style="darkgrid")
+
+x = [i for i in range(100)]
+y = [i for i in range(100)]
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('index.html',prediction_text='Employee Salary should be')
+    return render_template('index.html', prediction_text='Employee Salary should be')
 
+
+@app.route('/visualize')
+def visualize():
+    sns.lineplot(x, y)
+    canvas = FigureCanvas(fig)
+    img = io.BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img,mimetype='img/png')
 
 
 if __name__ == "__main__":
